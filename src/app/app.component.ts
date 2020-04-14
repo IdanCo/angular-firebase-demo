2ecc1ff8eac3b38055f688bbb0314d641f663b64
Idan Cohen <idan@idancohen.com>
2020-04-15 01:25:25 +0300

    setup basic chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore, QueryFn} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+
+interface Message {
+  text: string;
+  date?: Date;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-firebase-demo';
+export class AppComponent implements OnInit {
+  messages$: Observable<Message[]>;
+
+  constructor(private db: AngularFirestore) { }
+
+  ngOnInit() {
+    const queryFn: QueryFn = ref => ref.orderBy('date', 'desc');
+    this.messages$ = this.db.collection<Message>('messages', queryFn).valueChanges();
+  }
+
+  onButtonClicked(inputElement: HTMLInputElement) {
+    const text = inputElement.value;
+    const date = new Date();
+
+    if (!text) return;
+
+    this.db
+      .collection('messages')
+      .add({ text, date })
+      .then(docRef => console.log('Document written with ID: ', docRef.id))
+      .catch(error => console.error('Error adding document: ', error));
+
+    inputElement.value = '';
+  }
 }
