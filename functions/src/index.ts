fad0b5ea33be813df473603de3b94ea62f10d281
Idan Cohen <idan@idancohen.com>
2020-04-15 01:57:30 +0300

    create cloud function to add exclamation marks

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,16 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+export const addExclemation = functions.firestore
+  .document('messages/{messageId}')
+  .onCreate((snapshot: functions.firestore.DocumentSnapshot, context: functions.EventContext) => {
+    // Get an object representing the document
+    const newValue = snapshot.data();
+
+    // access the text field and add an exclamation mark
+    const text = newValue!.text + '!';
+
+    // return a promise to set the newly created document
+    return snapshot.ref.set({ text }, {merge: true});
+  });
