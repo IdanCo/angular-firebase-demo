087d4e5c029ac637f1c47165662c69e33d483ebe
Idan Cohen <idan@idancohen.com>
2020-04-15 00:35:36 +0300

    initialize firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAnalyticsModule} from '@angular/fire/analytics';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBH8muZo8N3RWIa9Z-EYvh3xYTtY8d1nk8',
+    authDomain: 'angular-firebase-demo-aec89.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-demo-aec89.firebaseio.com',
+    projectId: 'angular-firebase-demo-aec89',
+    storageBucket: 'angular-firebase-demo-aec89.appspot.com',
+    messagingSenderId: '1087489410952',
+    appId: '1:1087489410952:web:444f0d20e8e14d93445c3d',
+    measurementId: 'G-8FE7JSXKJR'
+  }
 };
