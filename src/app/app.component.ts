3c51898f2db22439f9ee227298fa7f50db8b3dbd
Idan Cohen <idan@idancohen.com>
2020-10-25 20:15:54 +0200

    show messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, QueryFn } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 interface Message {
   text: string;
@@ -11,20 +12,31 @@ interface Message {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  messages$: Observable<Message[]>;
 
   constructor(private db: AngularFirestore) {
+  }
+
+  ngOnInit(): void {
+    this.messages$ = this.db
+      .collection<Message>(
+        'messages',
+        ref => ref.orderBy('date', 'desc')
+      ).valueChanges();
   }
 
   onSend(message: HTMLInputElement): void {
     const text = message.value;
     const date = new Date();
 
-    this.db.collection<Message>('messages').add({
-      text,
-      date
-    }).then(docRef => console.log('Document written with ID: ', docRef.id))
-      .catch(error => console.error('Error adding document: ', error));
+    this.db
+      .collection<Message>('messages')
+      .add({
+        text,
+        date
+      }).then(docRef => console.log('Document written with ID: ', docRef.id))
+        .catch(error => console.error('Error adding document: ', error));
 
     message.value = ''; // clear input field
   }
